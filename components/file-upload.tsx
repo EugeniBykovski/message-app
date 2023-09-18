b9e791fab297de55c866d38ecdd53e63f2a3520a
EugeniBykovski <bykovskieugeni@mail.ru>
2023-09-18 14:48:51 +0200

    added message attachements

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { UploadDropzone } from "@/lib/uploading";
-import { X } from "lucide-react";
+import { FileIcon, X } from "lucide-react";
 import Image from "next/image";
 import "@uploadthing/react/styles.css";
 
@@ -20,6 +20,29 @@ const FileUpload = ({ value, endpoint, onChange }: FileUploadProps) => {
         <Image fill src={value} alt="Upload" className="rounded-full" />
         <button
           className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
+          type="button"
+          onClick={() => onChange("")}
+        >
+          <X className="w-4 h-4" />
+        </button>
+      </div>
+    );
+  }
+
+  if (value && fileType === "pdf") {
+    return (
+      <div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10">
+        <FileIcon className="w-10 h-10 fill-indigo-200 stroke-indigo-400 hover:underline" />
+        <a
+          href={value}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="ml-2 text-sm text-indigo-50 dark:text-indigo-400 hover:underline"
+        >
+          {value}
+        </a>
+        <button
+          className="bg-rose-500 text-white p-1 rounded-full absolute -top-2 -right-2 shadow-sm"
           type="button"
           onClick={() => onChange("")}
         >
